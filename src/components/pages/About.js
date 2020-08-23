b4875cb9e25c9cfca8db126d83247b6ed6832f2d
Alafia-Ojo <ojoalafiadavid@gmail.com>
2020-08-23 13:26:32 +0100

    Added a React app

---
+++ src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function About() {
+  return (
+    <React.Fragment>
+    <h1>About</h1>
+    <p>This i the ToDoList app version 2.0.0</p>
+    
+    </React.Fragment>
+    )
+
+}
+
+
+export default About;
